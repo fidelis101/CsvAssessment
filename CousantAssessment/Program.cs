b8f782d67f8ade0e92cb0a7106fbdf087da52bc2
Prime <ugwufidelis@ymail.com>
2020-07-27 12:11:35 +0100

    Adding News Api

--- CousantAssessment/Program.cs
+++ CousantAssessment/Program.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
 
 namespace CousantAssessment
 {
@@ -32,8 +35,36 @@ namespace CousantAssessment
             foreach (var foodCategory in r.OrderByDescending(r => r.Count))
                 Console.WriteLine(foodCategory.Category + " ........ " + foodCategory.Count);
 
+            GetMondayNews();
+            Console.ReadKey();
+
         }
 
+        public static void GetMondayNews()
+        {
+            using(HttpClient client = new HttpClient())
+            {
+                string newsUri = "https://newsapi.org/v2/everything?q=nigeria&apiKey=3574206693d14c8996dbe331bcbc22ad";
+                var response = client.GetAsync(newsUri).Result;
+                var result = JsonConvert.DeserializeObject<ApiResponse>(response.Content.ReadAsStringAsync().Result);
 
+                foreach (var item in result.articles)
+                {
+                    Console.WriteLine(item.title + " ........ " + item.publishedAt);
+                }
+            }
+        }
+
+        public class ApiResponse
+        {
+            public string status { get; set; }
+            public string totalResults { get; set; }
+            public Article[] articles { get; set; }
+        }
+        public class Article
+        {
+            public string title { get; set; }
+            public string publishedAt { get; set; }
+        }
     }
 }
